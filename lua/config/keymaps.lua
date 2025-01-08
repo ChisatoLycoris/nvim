1ebd11b6a056e3879db2d08916f16bfe02ff1523
ChisatoLycoris <changarm429@gmail.com>
2025-01-09 02:16:30 +0800

    add markdown preview plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- markdown preview
+map("n", "<leader>mpt", "<cmd>MarkdownPreviewToggle<cr>", { desc = "Markdown Preview Toggle" })
+map("n", "<leader>mpu", "<cmd>MarkdownPreview<cr>", { desc = "Markdown Preview Start" })
+map("n", "<leader>mpd", "<cmd>MarkdownPreviewStop<cr>", { desc = "Markdown Preview Stop" })
